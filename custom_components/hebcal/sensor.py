a08c3576a98a223e0016e028bfcae840b4bccdb3
Yuval Aboulafia <yuval.abou@gmail.com>
2020-10-07 11:47:34 +0300

    Docstings and decorators

--- custom_components/hebcal/sensor.py
+++ custom_components/hebcal/sensor.py
@@ -1,7 +1,5 @@
 """
 Platform to get Hebcalh Times And Hebcalh information for Home Assistant.
-
-Document will come soon...
 """
 import logging
 import json
@@ -70,7 +68,7 @@ class Hebcal(Entity):
 
     def __init__(self, hass, sensor_type, timezone, latitude, longitude,
                  havdalah, time_before, time_after):
-        """Initialize the sensor."""
+        """Initialize the sensor"""
         self.type = sensor_type
         self.entity_id = async_generate_entity_id(
             ENTITY_ID_FORMAT,
@@ -87,27 +85,27 @@ class Hebcal(Entity):
         self.holiday_name = None
 
     @property
-    def name(self):
-        """Return the name of the sensor."""
+    def name(self) -> str:
+        """Return the name of the sensor"""
         return SENSOR_PREFIX + SENSOR_TYPES[self.type][2]
 
     @property
     def icon(self):
-        """Icon to use in the frontend, if any."""
+        """Icon to use in the frontend"""
         return SENSOR_TYPES[self.type][1]
 
     @property
-    def should_poll(self):
+    def should_poll(self) -> bool:
         """Return true if the device should be polled for state updates"""
         return True
 
     @property
     def state(self):
-        """Return the state of the sensor."""
+        """Return the state of the sensor"""
         return self._state
 
     async def async_update(self):
-        """Update our sensor state."""
+        """Update our sensor state"""
         await self.update_db()
         type_to_func = {
             'shabbat_in': self.get_shabbat_time_in,
@@ -124,7 +122,7 @@ class Hebcal(Entity):
         await self.async_update_ha_state()
 
     async def create_db_file(self):
-        """Create the json db."""
+        """Create a json db"""
         #self.set_days()
         self.friday = datetime.date.today()
         self.hebcal_db = []
@@ -174,6 +172,7 @@ class Hebcal(Entity):
                       separators=None, default=None, sort_keys=True)
 
     async def filter_db(self, temp_db, state):
+        """Filters the database"""
         if state == "new":
             for extract_data in temp_db:
                 if "date" in extract_data:
@@ -233,7 +232,7 @@ class Hebcal(Entity):
                     self.parashat = extract_data['hebrew']
 
     async def update_db(self):
-        """Update the db."""
+        """Updates the db"""
         if not (pathlib.Path(self.config_path + 'hebcal_data.json').is_file()):
             await self.create_db_file()
         elif not self.hebcal_db or self.file_time_stamp is None:
@@ -247,13 +246,13 @@ class Hebcal(Entity):
 
     @callback
     def set_days(self):
-        """Set the friday and saturday."""
+        """Set the Friday and Saturday"""
         weekday = self.set_friday(datetime.date.today().isoweekday())
         self.friday = datetime.date.today() + datetime.timedelta(days=weekday)
 
     @classmethod
     def set_friday(cls, day):
-        """Set friday day."""
+        """Set Friday day"""
         switcher = {
             7: 5,
             1: 4,
@@ -265,43 +264,37 @@ class Hebcal(Entity):
         }
         return switcher.get(day)
 
-    # get shabbat entrace
     async def get_shabbat_time_in(self):
-        """Get shabbat entrace."""
+        """Get Shabbat entrance time"""
         if self.shabbat_in:
             return self.is_time_format(str(self.shabbat_in)[11:16])
         return self.shabbat_in
 
-    # get shabbat time exit
     async def get_shabbat_time_out(self):
-        """Get shabbat time exit."""
+        """Get Shabbat exit time"""
         if self.shabbat_out:
             return self.is_time_format(str(self.shabbat_out)[11:16])
         return self.shabbat_out
 
-    # get shabbat entrace
     async def get_holiday_time_in(self):
-        """Get shabbat entrace."""
+        """Get Shabbat entrance time"""
         if self.holiday_in:
             return self.is_time_format(str(self.holiday_in)[11:16])
         return self.holiday_in
 
-    # get shabbat time exit
     async def get_holiday_time_out(self):
-        """Get shabbat time exit."""
+        """Get Shabbat exit time"""
         if self.holiday_out:
             return self.is_time_format(str(self.holiday_out)[11:16])
         return self.holiday_out
 
-    # get parashat hashavo'h
-    async def get_parasha(self):
+    async def get_parasha(self) -> str:
         """Get parashat hashavo'h."""
         result = 'שבת מיוחדת'
         return self.parashat if self.parashat is not None else result
 
-    # check if is shabbat now / return true or false
-    async def is_shabbat(self):
-        """Check if is shabbat now / return true or false."""
+    async def is_shabbat(self) -> bool:
+        """Check if it is Shabbat now"""
         if self.shabbat_in is not None and self.shabbat_out is not None:
             is_in = self.shabbat_in - datetime.timedelta(
                 minutes=int(self._time_before))
@@ -312,9 +305,8 @@ class Hebcal(Entity):
             return 'False'
         return 'False'
 
-    # check if is holiday now / return true or false
-    async def is_holiday(self):
-        """Check if is holiday now / return true or false."""
+    async def is_holiday(self) -> bool:
+        """Check if it is Holiday now"""
         if self.holiday_in is not None and self.holiday_out is not None:
             is_in = self.holiday_in - datetime.timedelta(
                 minutes=int(self._time_before))
@@ -325,8 +317,8 @@ class Hebcal(Entity):
             return 'False'
         return 'False'
 
-    async def get_holiday_name(self):
-        """Get holiday name."""
+    async def get_holiday_name(self) -> str:
+        """Get holiday name"""
         result = self.heb_day_str()
         for extract_data in self.hebcal_db:
             if any(x in ["holiday", "roshchodesh"] for x in list(extract_data.values())):
@@ -337,15 +329,14 @@ class Hebcal(Entity):
                     return result
         return result
 
-    # convert to hebrew date
-    async def get_hebrew_date(self):
-        """Convert to hebrew date."""
+    async def get_hebrew_date(self) -> str:
+        """Convert to hebrew date"""
         day = self.heb_day_str()
         return day + self.hebcal_db[-1]['hebrew']
 
     @classmethod
-    def heb_day_str(cls):
-        """Set hebrew day."""
+    def heb_day_str(cls) -> str:
+        """Set hebrew day"""
         switcher = {
             7: "יום ראשון, ",
             1: "יום שני, ",
@@ -357,10 +348,9 @@ class Hebcal(Entity):
         }
         return switcher.get(datetime.datetime.today().isoweekday())
 
-    # check if the time is correct
     @classmethod
-    def is_time_format(cls, input_time):
-        """Check if the time is correct."""
+    def is_time_format(cls, input_time) -> str:
+        """Check if the time is correct"""
         try:
             time.strptime(input_time, '%H:%M')
             return input_time
