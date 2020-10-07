e67fa666e11b1e27e2cbff0c98c14a3bf0dead34
Yuval Aboulafia <yuval.abou@gmail.com>
2020-10-07 11:30:26 +0300

    Move constants to const.py

--- custom_components/hebcal/sensor.py
+++ custom_components/hebcal/sensor.py
@@ -10,45 +10,19 @@ import pathlib
 import datetime
 import time
 import aiohttp
-import voluptuous as vol
-from homeassistant.components.sensor import PLATFORM_SCHEMA
-import homeassistant.helpers.config_validation as cv
 from homeassistant.const import (
-    CONF_LATITUDE, CONF_LONGITUDE, CONF_RESOURCES)
+    CONF_LATITUDE, CONF_LONGITUDE, CONF_RESOURCES
+    )
 from homeassistant.helpers.entity import Entity
 from homeassistant.core import callback
 from homeassistant.helpers.entity import async_generate_entity_id
 from homeassistant.components.sensor import ENTITY_ID_FORMAT
 
+from .const import (
+    HAVDALAH_MINUTES, TIME_BEFORE_CHECK, TIME_AFTER_CHECK,SENSOR_TYPES, SENSOR_PREFIX
+    )
+
 _LOGGER = logging.getLogger(__name__)
-
-SENSOR_PREFIX = 'Hebcal '
-HAVDALAH_MINUTES = 'havdalah_calc'
-TIME_BEFORE_CHECK = 'time_before_check'
-TIME_AFTER_CHECK = 'time_after_check'
-
-SENSOR_TYPES = {
-    'shabbat_in': ['כניסת השבת', 'mdi:candle', 'shabbat_in'],
-    'shabbat_out': ['צאת השבת', 'mdi:exit-to-app', 'shabbat_out'],
-    'is_shabbat': ['האם שבת', 'mdi:candle', 'is_shabbat'],
-    'parasha': ['פרשת השבוע', 'mdi:book-open-variant', 'parasha'],
-    'holiday_in': ['כניסת החג', 'mdi:candle', 'holiday_in'],
-    'holiday_out': ['צאת החג', 'mdi:exit-to-app', 'holiday_out'],
-    'is_holiday': ['האם חג', 'mdi:candle', 'is_holiday'],
-    'holiday_name': ['שם החג', 'mdi:book-open-variant', 'holiday_name'],
-    'hebrew_date': ['תאריך עברי', 'mdi:calendar', 'hebrew_date'],
-}
-
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Optional(CONF_LATITUDE): cv.latitude,
-    vol.Optional(CONF_LONGITUDE): cv.longitude,
-    vol.Optional(HAVDALAH_MINUTES, default=42): int,
-    vol.Optional(TIME_BEFORE_CHECK, default=10): int,
-    vol.Optional(TIME_AFTER_CHECK, default=10): int,
-    vol.Optional(CONF_RESOURCES, default=['shabbat_in', 'shabbat_out', 'parasha', 'hebrew_date', 'is_shabbat',
-                                          'holiday_in', 'holiday_out', 'is_holiday', 'holiday_name']):
-        vol.All(cv.ensure_list, [vol.In(SENSOR_TYPES)]),
-})
 
 
 async def async_setup_platform(
